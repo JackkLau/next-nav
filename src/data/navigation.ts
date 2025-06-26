beac3e874da8be6ebb48c9b26cbe705d02ac177a
Jackklau <287693621@qq.com>
2025-06-26 10:00:00 +0800

    fix: lint

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -167,6 +167,13 @@ export const navigationData: NavigationItem[] = [
     category: CategoryType.c,
     description: 'Google Analytics流量统计分析系统',
   },
+  {
+    name: 'AI工具集',
+    url: 'https://ai-bot.cn/',
+    imgUrl: '/nav/15.png',
+    category: CategoryType.g,
+    description: 'AI工具集官网收录了国内外数百个AI工具，该导航网站包括AI写作工具、AI图像生成和背景移除、AI视频制作、AI音频转录、AI辅助编程、AI音乐生成、AI绘画设计、AI对话聊天等AI工具集合大全，以及AI学习开发的常用网站、框架和模型，帮助你加入人工智能浪潮，自动化高效完成任务！',
+  },
 
 
 

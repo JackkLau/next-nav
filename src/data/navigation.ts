ea079665299eb99974013e0edd8c503e7c811a28
Jackklau <287693621@qq.com>
2025-07-12 17:50:11 +0800

    data

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -359,7 +359,7 @@ export const navigationData: NavigationItem[] = [
   },
   {
     name: '美图宝库',
-    url: 'https://pic.loverezhao.top/',
+    url: 'https://www.meituhd.com/',
     imgUrl: '/nav/41.png',
     category: CategoryType.e,
     description: '美图宝库网站，提供海量高清壁纸资源，动漫、游戏、美女、风景等壁纸类型应有尽有。轻松下载心仪壁纸，装点您的电脑。壁纸大全任你挑选，尽在美图宝库！',
